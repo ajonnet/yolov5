bc36cc6e36026234decb137a68b8e0bef18fd9d8
Amit Jain <ajonnet@gmail.com>
2021-12-09 22:10:18 +0530

    refactor the code

--- detect.py
+++ detect.py
@@ -97,7 +97,7 @@ def detect(save_img=False):
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         with open(txt_path + '.txt', 'a') as f:
-                            f.write(("{}," * 8).format(dataset.frame, detId, cls, conf, xyxy[0], xyxy[1], xyxy[2], xyxy[3]))
+                            f.write(("{}," * 8).format(dataset.frame, detId, cls, conf, *xyxy))
                             f.write('\n')
                             # f.write(('%g ' * 5 + '\n') % (cls, *xywh))  # label format
 
